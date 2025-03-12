1edb9b2a9ff51da7cd7a688d6d4d6e76bdb15a70
Shobo Muhammed <shobomuhammed23@gmai.com>
2025-03-12 20:15:42 +0930

    feat: added 404 route and default route

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-const Express = require('express')
-const route = require('./routes/routes')
-const connectDb = require('./config/billConfiq')
-require('dotenv/config')
-const app = Express()
-app.use(Express.json())
+const Express = require("express");
+const route = require("./routes/routes");
+const connectDb = require("./config/billConfiq");
+require("dotenv/config");
+const app = Express();
+app.use(Express.json());
 
-const {PORT} = process.env
+const { PORT } = process.env;
 
-connectDb()
+connectDb();
 
-app.use('/api', route)
+app.use("/api", route);
+
+app.all("/", (req, res) => {
+  return res.status(200).json({ message: "api up and running" });
+});
+
+app.all("*", (req, res) => {
+  return res.status(404).json({ message: "route does not exist" });
+});
 
 app.listen(PORT, () => {
-  console.log((new Date().toLocaleDateString()), PORT);
-})
+  console.log(new Date().toLocaleDateString(), PORT);
+});
